d0835cb78f925d029cb7ddb1f087e485d9820edd
ngoctran31231024952 <ngoctran.31231024952@st.ueh.edu.vn>
2024-08-12 16:13:44 +0700

    Code Day2

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello Tram Xink eu");
+        Console.ReadKey();
+    }
+}
