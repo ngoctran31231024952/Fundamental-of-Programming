c7b0aec1eaa82461d9e41356458df08208f007f3
ngoctran31231024952 <ngoctran.31231024952@st.ueh.edu.vn>
2024-08-13 18:35:41 +0700

    Delete Program.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello Tram Xink eu");
-        Console.ReadKey();
-    }
-}
