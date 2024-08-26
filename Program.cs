347e08cb558c999658954680228b2a0b2c2225e6
ngoctran31231024952 <ngoctran.31231024952@st.ueh.edu.vn>
2024-08-26 16:16:06 +0700

    BT code ngay 26-8

--- Program.cs
+++ Program.cs
@@ -2,28 +2,62 @@
 {
     private static void Main(string[] args)
     {
-        Baitap1();
-        Baitap2();       
-        
-    }
-        static void Baitap1()
-    {
-        Console.WriteLine("Baitap1:");
-        Console.WriteLine("Enter degree in Celsius!");
-        var userinput = Console.ReadLine();
-        float Celsius = Convert.ToSingle(userinput);
-        Console.WriteLine($"Degree in Fahrenheit:{Celsius * 1.8 + 32}");
-        Console.WriteLine($"Degree in Kelvin:{Celsius + 273}");
-    }
-        static void Baitap2()
-    {
-        Console.WriteLine("Baitap2:");
-        const double Pi = Math.PI;
-        Console.Write("Enter sphere's radius:");
-        float r = Convert.ToSingle(Console.ReadLine());
-        double s = 4 * Pi * Math.Pow(r, 2);
-        double v = 4 / 3 * Pi * Math.Pow(r, 3);
-        Console.WriteLine($"Surface={s}");
-        Console.WriteLine($"Volume={v}");
+        int Thua = 0;
+        int Thang = 0;
+        int Choi = 0;
+        int SoTienConLai;
+        do
+        {
+            Random rnd = new Random();
+            int comp_num = rnd.Next(1, 100);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Your number:");
+                int man_num = int.Parse("0" + Console.ReadLine());
+                if (man_num == comp_num)
+                {
+                    Console.WriteLine("Bravo! You are genious");
+                    Console.WriteLine("You win!");
+                    Thang = Thang + 1;
+                    break;
+                }
+                else
+                if (i == 4)
+                {
+                    if (man_num < comp_num)
+                    {
+                        Console.WriteLine("Your guessing number is less than computer number");
+                        Console.WriteLine("You lose!");
+                        Thua = Thua + 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your guessing number is greater than computer number");
+                        Console.WriteLine("You lose!");
+                        Thua = Thua + 1;
+                    }
+                }
+                else
+                if (man_num < comp_num)
+                    Console.WriteLine("Your guessing number is less than computer number");
+                else
+                    Console.WriteLine("Your guessing number is greater than computer number");
+            }
+            Console.WriteLine("Do you want to continue? <Y/N>");
+            String res = "" + Console.ReadLine();
+            if (res.ToUpper().Equals("N"))
+            {
+                Console.WriteLine("Bye! See you again");
+                Choi = Choi + 1;
+                break;
+            }
+            else
+                Choi= Choi + 1;
+        }
+        while (true);
+        Console.WriteLine($"So lan da choi:{Choi}");
+        Console.WriteLine($"So tien con lai: {1000 - Choi * 50}");
+        Console.WriteLine($"So lan thang/thua: {Thang}/{Thua}");
+
     }
 }
